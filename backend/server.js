daf7cb77b24810c77abec88b31db8a2a07e5b827
Chetan Kaushik <kaushikchetan199@gmail.com>
2022-01-23 16:02:58 +0530

    Database connection done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
+require('dotenv').config({path: 'backend/config/config.env'});
 const app = require('./app');
+const connectDB = require('./config/database');
+
+// connect to database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
