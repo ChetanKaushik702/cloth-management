e2bfee466d99ec6e6432e5a128a5f32a56f2454c
Chetan Kaushik <kaushikchetan199@gmail.com>
2022-01-24 11:02:05 +0530

    Created schema for cloth

---
+++ backend/models/clothModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const clothSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Please enter name for the outfit']
+    },
+    price: {
+        type: Number,
+        min: 0,
+        required: [true, 'Please enter outfit price']
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        required: [true, 'Please enter outfit quantity']
+    },
+    brand: {
+        type: String,
+        trim: true,
+        required: [true, 'Please enter the outfit brand']
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            } 
+        }
+    ],
+    category: {
+        type: String,
+        trim: true,
+        required: [true, 'Please enter the outfit category']
+    }
+});
+
+module.exports = mongoose.model('outfit', clothSchema);
