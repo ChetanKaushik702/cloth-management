123f826837de3b273b73b2b42ed79be7c32d5efe
Chetan Kaushik <kaushikchetan199@gmail.com>
2022-01-24 19:04:03 +0530

    CRUD operations implemented

--- backend/controllers/clothController.js
+++ backend/controllers/clothController.js
@@ -34,3 +34,74 @@ exports.addOutfit = AsyncErrorHandler(async (req, res, next) => {
         outfit
     });
 })
+
+// update cloth details: ADMIN route
+exports.updateDetails = AsyncErrorHandler(async (req, res, next) => {
+    const id = req.query.id;
+    let outfit = await Outfit.findById(id);
+    if (!outfit) {
+        return next(new ErrorHandler("Product not found!", 404));
+    }
+
+    if (req.files !== null) {
+        // deleting images from cloudinary
+        for (let i = 0; i < outfit.images.length; i++) {
+          await cloudinary.v2.uploader.destroy(outfit.images[i].public_id);
+        }
+    
+        const imageLinks = [];
+
+        for (let i = 0; i < req.files.File.length; i++) {
+            const result = await cloudinary.v2.uploader.upload(req.files.File[i].tempFilePath, {
+                folder: "cloth"
+            });
+
+            imageLinks.push({
+                public_id: result.public_id,
+                url: result.secure_url
+            });
+        }
+        
+        req.body.images = imageLinks;
+    }
+    
+    outfit = await Outfit.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        findAndModify: false,
+      });
+    res.status(200).json({
+        success: true,
+        outfit,
+    });
+})
+
+// delete cloth
+exports.deleteCloth = AsyncErrorHandler(async (req, res, next) => {
+    const id = req.query.id;
+    const outfit = await Outfit.findOne({id});
+    
+    if (!outfit) {
+        return next(new ErrorHandler('Product not found!!', 404));
+    }
+
+    for (let i = 0; i < outfit.images.length; i++) {
+        await cloudinary.v2.uploader.destroy(outfit.images[i].public_id);
+    }
+
+    await outfit.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Product deleted successfully"
+    })
+})
+
+// get all clothes: USER ROUTE
+exports.getAllOutfits = AsyncErrorHandler(async(req, res, next) => {
+    const outfits = await Outfit.find();
+    res.status(200).json({
+        success: true,
+        outfits
+    })
+})
--- backend/routes/clothRoutes.js
+++ backend/routes/clothRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { addOutfit } = require('../controllers/clothController');
+const { addOutfit, updateDetails, deleteCloth, getAllOutfits } = require('../controllers/clothController');
 const { isAuthenticatedUser, isAuthorizedRole } = require('../middleware/auth');
 const router = express.Router();
 
 router.route('/add').post(isAuthenticatedUser, isAuthorizedRole('admin'), addOutfit);
+router.route('/update').put(isAuthenticatedUser, isAuthorizedRole('admin'), updateDetails);
+router.route('/delete').delete(isAuthenticatedUser, isAuthorizedRole('admin'), deleteCloth);
+router.route('/all').get(isAuthenticatedUser, getAllOutfits);
 
 module.exports = router;
